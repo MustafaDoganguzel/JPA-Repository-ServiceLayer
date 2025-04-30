c41bf8301a4115f90f54a9f72ff2c9990d107cfc
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-04-30 21:56:21 +0300

    Fruit class has been created with lombok and jpa annotations

---
+++ src/main/java/entity/Fruit.java
@@ -0,0 +1,25 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "fruit", schema = "fsweb")
+public class Fruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private Double price;
+    @Column(name = "is_grow_on_tree")
+    private Boolean isGrowOnTree;
+
+}
