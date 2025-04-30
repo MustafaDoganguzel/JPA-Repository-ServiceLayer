11195c9ac2232ccbfd8dd979fc87a4be93ce3ab5
MustafaDoganguzel <doganguzelmustafa1@gmail.com>
2025-05-01 01:12:42 +0300

    VegetableServiceImpl with basic CRUD and search methods

---
+++ src/main/java/com/workintech/s18d2/services/VegetableServiceImpl.java
@@ -0,0 +1,54 @@
+package com.workintech.s18d2.services;
+
+import com.workintech.s18d2.dao.VegetableRepository;
+import com.workintech.s18d2.entity.Vegetable;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+
+public class VegetableServiceImpl implements VegetableService{
+    private final VegetableRepository vr;
+
+    @Autowired
+    public VegetableServiceImpl(VegetableRepository vr){
+        this.vr = vr;
+    }
+
+
+    @Override
+    public List<Vegetable> getByPriceDesc() {
+        return vr.getByPriceDesc();
+    }
+
+    @Override
+    public List<Vegetable> getByPriceAsc() {
+        return vr.getByPriceAsc();
+    }
+
+    @Override
+    public List<Vegetable> searchByName(String name) {
+        return vr.searchByName(name);
+    }
+
+
+    public Vegetable save(Vegetable vegetable){
+        return vr.save(vegetable);
+    }
+
+    public Vegetable getById(Long id){
+        Optional<Vegetable> vegetableOptional = vr.findById(id);
+        if(vegetableOptional.isPresent()){
+            return vegetableOptional.get();
+        }else{
+            throw new RuntimeException("DEGISECEK"); // degistirilecek
+        }
+
+    }
+
+    public Vegetable delete(Long id){
+        Vegetable found = vr.getById(id);
+        vr.delete(found);
+        return found;
+    }
+}
